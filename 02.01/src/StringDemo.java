a9cf22cafb39e1100186518a88771e4966002ec9
Mi Notebook PRO <Mi Notebook PRO@10.1.32.137>
2022-02-01 18:24:57 -0800

    initial checking

---
+++ 02.01/src/StringDemo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		/*int a = 0;
+		
+		Scanner input = new Scanner(System.in);
+		String str1 = input.nextLine();
+		for(int n = 0; n<str1.length(); n++) {
+			if(str1.charAt(n) == ' ') {
+				System.out.println('0');}
+				*/
+		StringBuilder str = new StringBuilder("");
+		System.out.println(str.capacity());
+		System.out.println(str.length());
+			
+		}
+	}
